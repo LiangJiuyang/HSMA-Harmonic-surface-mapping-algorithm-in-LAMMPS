29a61d9841fee648142bcc62fc9bd449e8deddae
Jiuyang Liang <liangjiuyang@sjtu.edu.cn>
2021-11-14 18:52:45 +0800

    Add lib file, install.py, and makefile

--- lib/scafacos/Install.py
+++ lib/scafacos/Install.py
@@ -5,24 +5,25 @@ Install.py tool to download, unpack, build, and link to the ScaFaCoS library
 used to automate the steps described in the README file in this dir
 """
 
-from __future__ import print_function
+from __future__ import print_function #用比当前版本的 python 更新的版本的print函数
 import sys, os, subprocess, shutil, tarfile
-from argparse import ArgumentParser
+from argparse import ArgumentParser #参数解析器 用于将命令行中输入的参数传递给主函数
 
-sys.path.append('..')
+sys.path.append('..') #将上一级目录加入默认package搜索路径中
 from install_helpers import fullpath, geturl, get_cpus, checkmd5sum
 
-parser = ArgumentParser(prog='Install.py',
-                        description="LAMMPS library build wrapper script")
+parser = ArgumentParser(prog='Install.py', #表示文件名字
+                        description="LAMMPS library build wrapper script") #在帮助文件其他输出前输出这一句
 
 # settings
 
-version = "1.0.1"
-url = "https://github.com/scafacos/scafacos/releases/download/v%s/scafacos-%s.tar.gz" % (version, version)
+version = "1.0.1" #定义版本号
+url = "https://github.com/scafacos/scafacos/releases/download/v%s/scafacos-%s.tar.gz" % (version, version) #设置下载网址
+
 
 # known checksums for different ScaFaCoS versions. used to validate the download.
 checksums = { \
-        '1.0.1' : 'bd46d74e3296bd8a444d731bb10c1738' \
+        '1.0.1' : 'bd46d74e3296bd8a444d731bb10c1738' \ #字典 存储键和键值
         }
 
 # extra help message
@@ -41,15 +42,15 @@ make lib-scafacos args="-p $HOME/scafacos" # use existing ScaFaCoS installation 
 
 # parse and process arguments
 
-pgroup = parser.add_mutually_exclusive_group()
+pgroup = parser.add_mutually_exclusive_group() #添加互斥组 就是说下面这几个选项每次只能选择一个运行
 pgroup.add_argument("-b", "--build", action="store_true",
-                    help="download and build the ScaFaCoS library")
+                    help="download and build the ScaFaCoS library") #这段话的意思是，如果输入是"-b"或者"--build",那么将build的值变化为true，在帮助里会输出"download and build the ScaFaCoS library"
 pgroup.add_argument("-p", "--path",
-                    help="specify folder of existing ScaFaCoS installation")
+                    help="specify folder of existing ScaFaCoS installation") #好像是设置下载文件的路径
 parser.add_argument("-v", "--version", default=version,
-                    help="set version of ScaFaCoS to download and build (default: %s)" % version)
+                    help="set version of ScaFaCoS to download and build (default: %s)" % version) #设置下载的版本
 
-args = parser.parse_args()
+args = parser.parse_args() #将parser的参数赋给args
 
 # print help message and exit, if neither build nor path options are given
 if not args.build and not args.path:
